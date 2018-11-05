e8706755a596e469e55526c5ff51796b5b3c7292
korentyn <frantzcorentin@gmail.com>
2018-11-05 23:25:28 +0100

    swqdf

--- application/views/layout/layout.php
+++ application/views/layout/layout.php
@@ -14,8 +14,8 @@
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
         <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a class="orange-text text-darken-1" href="#">Recruter</a></li>
-            <li><a class="grey-text text-darken-4" href="#">Travailler</a></li>
+            <li><a class="orange-text text-darken-1" href="<?php echo site_url('index.php/Trouver/freelance'); ?>">Recruter</a></li>
+            <li><a class="grey-text text-darken-4" href="<?php echo site_url('index.php/Trouver/projets'); ?>">Travailler</a></li>
             <li><a class="grey-text text-darken-4" href="#">Comment ça marche ?</a></li>
         </ul>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
@@ -31,7 +31,7 @@
 
 </ul>
 
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <!--////////// LIBRAIRIES BOOTSTRAP 4.1 ////////////////-->
 <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>-->
 <script
---
+++ application/views/pages/liste_freelancer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coren
+ * Date: 05/11/2018
+ * Time: 22:52
+ */
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+Liste freelances
+</body>
+</html>
