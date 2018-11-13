957374f9865f0a138c164a30aa79fc95992af82c
korentyn <frantzcorentin@gmail.com>
2018-11-13 14:10:47 +0100

    Ajout page + lien bdd pour Creation Projet

--- application/controllers/Inscription.php
+++ application/controllers/Inscription.php
@@ -5,37 +5,30 @@ require (APPPATH.'/libraries/REST_Controller.php');
 class Inscription extends CI_Controller {
 
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('Userbdd');
-        $this->load->helper('url_helper');
-    }
+//    public function __construct()
+//    {
+//        parent::__construct();
+//
+//    }
 
     public function index()
     {
+        $this->load->model('Userbdd');
+        $this->load->helper('url_helper');
         $this->load->helper('url');
         $this->load->view('layout/layout');
         $this->load->view('pages/formulaireInscription');
         $this->load->view('layout/footer');
     }
-/*
-    public function view($page){
 
-        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-        {
-            // Whoops, we don't have a page for that!
-            show_404();
-        }
-
-        $data['title'] = ucfirst($page); // Capitalize the first letter
-
-        $this->load->view('layout/header', $data);
-        $this->load->view('pages/'.$page, $data);
-        $this->load->view('layout/footer', $data);
+    public function projet(){
+        $this->load->model('Projetbdd');
+        $this->load->helper('url_helper');
+        $this->load->helper('url');
+        $this->load->view('layout/layout');
+        $this->load->view('pages/formCreationProjet');
+        $this->load->view('layout/footer');
     }
-*/
-
 
 
 }
