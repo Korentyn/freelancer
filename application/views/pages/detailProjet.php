b3087d87396cc6ae2d21eecc89475619a26d43a5
korentyn <frantzcorentin@gmail.com>
2019-02-15 17:06:27 +0100

    Finalisation Vue detailProjet

--- application/views/pages/detailProjet.php
+++ application/views/pages/detailProjet.php
@@ -32,42 +32,40 @@
                 <div class="form-group">
                     <div class="input-group">
                         <div class="col col-md-2"><label for="select" class=" form-control-label">Titre</label></div>
-
                         <input type="text" id="titre" value="<?php echo $news['titre']; ?>" placeholder="Titre de votre projet" class="form-control" disabled>
                     </div>
                 </div>
                 <div class="form-group">
 
                     <div class="row form-group">
-                        <div class="col- col-md-3"><label for="textarea-input" class=" form-control-label">Présentation du projet</label></div>
-                        <div class="col-12 col-md-9"><textarea type="text" id="texte-presentation" value="" id="textarea-input" rows="9"  class="form-control" disabled><?php echo $news['presentation']; ?></textarea></div>
+                        <div class="col- col-md-3"><label for="textarea-input" class=" form-control-label">Votre réponse</label></div>
+                        <div class="col-12 col-md-9"><textarea type="text" id="texte-presentation" id="textarea-input" rows="9"  class="form-control" ><?php echo $news['titre']; ?></textarea></div>
                     </div>
 
                 </div>
                 <div class="row form-group">
                     <div class="col col-md-3"><label for="select" class=" form-control-label">Budget</label></div>
                     <div class="col-12 col-md-9">
-                        <input type="text" id="budget" value="<?php echo $news['description']; ?>" class="form-control" disabled>
-
+                        <input type="text" name="budget" value="<?php echo $news['budget']; ?>" class="form-control">
                     </div>
                 </div>
 
                 <div class="card-header"><strong></strong></div>
                 <br>
-                <div class="row form-group">
-                    <div class="col col-md-3"><label for="select" class=" form-control-label">Compétence demandé</label></div>
-                    <div class="col-12 col-md-9">
+				<div class="row form-group">
+					<div class="col col-md-3"><label for="select" class=" form-control-label">Compétence demandé</label></div>
+					<div class="col-12 col-md-9">
 
 
-                            <?php
-                                $techno = $news['techno'];
-                            if(isset($techno)) { ?>
-                                <input type="text" id="budget" value="<?php echo $techno; ?>" class="form-control" disabled>
-                            <?php }else{ ?>
-                        <input type="text" id="budget" value="Libre" class="form-control" disabled>
-                            <?php } ?>
-                    </div>
-                </div>
+						<?php
+						$techno = $news['techno'];
+						if(isset($techno)) { ?>
+							<input type="text" id="budget" value="<?php echo $techno; ?>" class="form-control" disabled>
+						<?php }else{ ?>
+							<input type="text" id="budget" value="Libre" class="form-control" disabled>
+						<?php } ?>
+					</div>
+				</div>
 
 				<div class="row form-group">
 					<div class="col col-md-3"><label for="select" class=" form-control-label">Mot clé</label></div>
@@ -86,14 +84,69 @@
 
             </div>
 
-            <div class="text-center form-actions form-group"><a href="<?php echo site_url('index.php/Projet/deviserProjet'); ?>?id=<?php echo $news['id']?>"  class="btn waves-effect waves-light orange accent-4">Deviser le projet
+            <div class="text-center form-actions form-group"><a href=""  class="btn waves-effect waves-light orange accent-4">Deviser le projet
 				</a>
             </div>
+			<?php } ?>
     </div>
     </form>
+
+
+<!------------------------------------------------------------------------------------------------------------------------>
+	<h2>Votre devis</h2>
+	<form action="<?php echo site_url('index.php/Projet/enregistrer'); ?>" method="post">
+		<div class="card-header"><strong><div class="createur-elem"><img src="<?php echo base_url()?>asset/images/avatar/<?php echo $news['image']; ?>" alt=""><?php echo $news['login']; ?></div></strong></div>
+		<div class="card-body card-block">
+
+
+			<div class="form-group">
+
+				<div class="row form-group">
+					<div class="col- col-md-3"><label for="textarea-input" class=" form-control-label">Présentation du projet</label></div>
+					<div class="col-12 col-md-9"><textarea type="text" id="texte-presentation" value="" id="textarea-input" rows="9"  class="form-control" disabled><?php echo $news['presentation']; ?></textarea></div>
+				</div>
+
+			</div>
+			<div class="form-group">
+				<div class="input-group">
+					<div class="col col-md-2"><label for="select" class=" form-control-label">Votre estimation du prix global</label></div>
+					<input type="text" name="prix" placeholder="en €" class="form-control" >
+				</div>
+			</div>
+			<div class="row form-group">
+				<div class="col col-md-3"><label for="select" class=" form-control-label">Votre estimation du nombre d'heures de développement</label></div>
+				<div class="col-12 col-md-9">
+					<input type="text" name="heures" class="form-control">
+
+				</div>
+			</div>
+			<div class="row form-group">
+				<div class="col col-md-3"><label for="select" class=" form-control-label">Votre tarif horaire</label></div>
+				<div class="col-12 col-md-9">
+					<input type="text" name="tarif_hor" class="form-control" disabled>
+
+				</div>
+			</div>
+			<div class="card-header"><strong></strong></div>
+			<br>
+			<div class="row form-group">
+				<div class="col col-md-3"><label for="select" class=" form-control-label">Compétence proposée</label></div>
+				<div class="col-12 col-md-9">
+					<input type="text" name="competence" value="" class="form-control" >
+				</div>
+			</div>
+
+		</div>
+
+		<div class="text-center form-actions form-group"><a href="<?php echo site_url('index.php/Projet/deviserProjet'); ?>?id=<?php echo $news['id']?>"  class="btn waves-effect waves-light orange accent-4">Deviser le projet
+			</a>
+		</div>
+
+		</div>
+	</form>
+	</form>
     </div>
-    </div>
-    <?php } ?>
+
 <script>
 
 </script>
